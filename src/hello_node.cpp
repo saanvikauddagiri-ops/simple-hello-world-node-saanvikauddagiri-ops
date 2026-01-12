d0ad9ad914bafbace8ef78c6184fa497376fddc5
Saanvika Uddagiri <saanviuddagiri@gmail.com>
2026-01-12 23:12:09 +0530

    Fix Hello World node final

--- src/hello_node.cpp
+++ src/hello_node.cpp
@@ -1,41 +1,24 @@
 #include <chrono>
-#include <functional>
 #include <memory>
-#include <string>
+
 #include "rclcpp/rclcpp.hpp"
 
 using namespace std::chrono_literals;
 
-/*
- * TODO: Create a Class named 'HelloNode' that inherits from rclcpp::Node.
- * Requirements:
- * 1. The constructor should name the node "hello_world_node".
- * 2. Create a timer that triggers every 1000ms (1 second).
- * 3. The timer callback should print "Hello, World!" using the RCLCPP_INFO macro.
- */
-
 class HelloNode : public rclcpp::Node
 {
 public:
-  HelloNode()
-  : Node("hello_world_node")
+  HelloNode() : Node("hello_world_node")
   {
-    // TODO: Initialize the timer here
     timer_ = this->create_wall_timer(
-  1000ms,
-  std::bind(&HelloNode::timer_callback, this)
-);
-
+      1000ms,
+      [this]() {
+        RCLCPP_INFO(this->get_logger(), "Hello, World!");
+      }
+    );
   }
 
 private:
-  // TODO: Define the timer_callback function here
-void timer_callback()
-{
-  RCLCPP_INFO(this->get_logger(), "Hello, World!");
-}
-
-
   rclcpp::TimerBase::SharedPtr timer_;
 };
 
@@ -46,4 +29,3 @@ int main(int argc, char * argv[])
   rclcpp::shutdown();
   return 0;
 }
-
