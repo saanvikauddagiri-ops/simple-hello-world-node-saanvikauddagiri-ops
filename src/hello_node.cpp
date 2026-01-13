af82d394b1000ae5b7bc41d687bb52992677bd02
Saanvika Uddagiri <saanviuddagiri@gmail.com>
2026-01-13 14:07:30 +0530

    Print Hello World once

--- src/hello_node.cpp
+++ src/hello_node.cpp
@@ -1,11 +1,7 @@
-#include <rclcpp/rclcpp.hpp>
+
 #include <iostream>
 
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  std::cout << "Hello, World!" << std::endl;
-  rclcpp::shutdown();
-  return 0;
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
-
